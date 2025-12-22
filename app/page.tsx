135b627ab7c26e92b8239e0e45162ad45223d921
ahmednader515 <ahmednader5012@gmail.com>
2025-12-22 13:49:41 +0200

    v2.6

--- app/page.tsx
+++ app/page.tsx
@@ -278,7 +278,7 @@ export default function HomePage() {
               {t("homepage.successStartsHere")}
             </p>
             <Button size="lg" asChild className="bg-brand hover:bg-brand/90 text-white">
-              <Link href="/sign-up">
+              <Link href="/sign-in">
                 {t("homepage.startNow")} <ArrowRight className="mr-2 h-4 w-4" />
               </Link>
             </Button>
